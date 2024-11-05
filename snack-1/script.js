05a84543d3594e8f71e7429a94ae3f0922497b1f
GiorgiaSecchi <secchigrg@gmail.com>
2024-11-05 14:49:04 +0100

    1) snack

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,7 @@
-const names = ['Edoardo', 'Simone', 'Francesco'];
+const names = ["Edoardo", "Simone", "Francesco"];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+console.log(names);
+console.log(names.join(`, `));
