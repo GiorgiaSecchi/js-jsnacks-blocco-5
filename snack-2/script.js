b4c4ca0e421dd8806fbcb1b06fbc1468186f0fce
GiorgiaSecchi <secchigrg@gmail.com>
2024-11-05 15:12:08 +0100

    2) snack

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,8 +1,16 @@
 const people = [
-  { name: 'Paolo', age: 35 },
-  { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
+  { name: "Paolo", age: 35 },
+  { name: "Giulia", age: 24 },
+  { name: "Marco", age: 67 },
 ];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+const names = people.map((person) => person.name).join(`, `);
+console.log(names);
+
+// const names = people.map((person) => person.name);
+// console.log(names);
+
+// people.forEach((person) => console.log(person.name));
